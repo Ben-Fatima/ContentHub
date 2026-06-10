b88e41d5c8ebed4345995e3781b11170dbfd21ad
Benhammou Fatima zahra <fz.benhammou@Viveris.lan>
2026-06-10 12:54:47 +0200

    feat(#1): project setup with Spring Boot, package structure, and health endpoint

---
+++ src/test/java/com/fzbenhammou/contenthub/ContenthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fzbenhammou.contenthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContenthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
